cc30a5ef45b7289e02b899f25084bbff8f570dba
Tyler Lee <wtleeiv@gmail.com>
2018-02-27 18:49:53 -0800

    reorder initial startup

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,7 +1,16 @@
+;; don't flash menu bars initially
+(menu-bar-mode 0)
+(blink-cursor-mode 0)
+(scroll-bar-mode 0)
+(tool-bar-mode 0)
+
 ;; steps required by melpa to add package source
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (package-initialize)
+
+;; put here for config.org
+(setq vc-follow-symlinks t)
 
 ;; load config file
 (org-babel-load-file "~/.emacs.d/config.org")
