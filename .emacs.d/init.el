c39fb4514e2f2ed06e09b8c3f0c8aaad86de499e
Tyler Lee <tlee2@illumina.com>
2018-03-01 10:08:11 -0800

    secure melpa

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,7 +6,8 @@
 
 ;; steps required by melpa to add package source
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
 ;; put here for config.org
@@ -14,15 +15,3 @@
 
 ;; load config file
 (org-babel-load-file "~/.emacs.d/config.org")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (pdf-tools use-package noctilux-theme))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
