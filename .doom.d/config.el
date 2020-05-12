e3ad83aebdaf507eae6d3625149e07e3b7057117
Lee <wtlee2@nasmac3195.ndc.nasa.gov>
2020-05-12 09:55:33 -0700

    add .doom.d

---
+++ .doom.d/config.el
@@ -0,0 +1,82 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Tyler Lee"
+      user-mail-address "wtleeiv@gmail.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+(setq doom-font (font-spec :family "Menlo" :size 12))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type 'relative)
+
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
+;; they are implemented.
+
+(after! tramp
+  (add-to-list 'tramp-remote-path 'tramp-own-remote-path))
+
+(when (eq window-system 'ns) ; only on mac
+  (setq ns-command-modifier 'control
+        ns-control-modifier 'meta
+        ns-option-modifier 'super))
+
+(defun toggle-transparency ()
+  (interactive)
+  (let ((alpha (frame-parameter nil 'alpha)))
+    (set-frame-parameter
+     nil 'alpha
+     (if (or (null alpha) ; initial load
+             (eql (cond ((numberp alpha) alpha)
+                        ((numberp (cdr alpha)) (cdr alpha))
+                        ;; Also handle undocumented (<active> <inactive>) form.
+                        ((numberp (cadr alpha)) (cadr alpha)))
+                  100))
+         '(85 . 75) '(100 . 100)))))
+
+(global-set-key (kbd "C-c t") 'toggle-transparency)
+
+;; (toggle-frame-maximized) ; M-f10
+;; (set-frame-parameter (selected-frame) 'alpha '(85 . 75))
+;; (add-to-list 'default-frame-alist '(alpha . (85 . 75)))
+
+;; (add-hook 'emacs-startup-hook #'(lambda () (message "Happy Hackses :)")))
