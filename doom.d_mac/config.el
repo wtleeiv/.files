d1ab547bdc6d07c16750d1990f76a69678d64bc5
Lee <wtlee2@nasmac3431.ndc.nasa.gov>
2020-09-05 14:24:15 -0700

    readable font spec

--- doom.d_mac/config.el
+++ doom.d_mac/config.el
@@ -21,7 +21,10 @@
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-(setq doom-font (font-spec :family "Iosevka" :size 13 :slant 'italic :weight 'light))
+(setq doom-font (font-spec :family "Iosevka"
+                           :size 13
+                           :slant 'italic
+                           :weight 'light))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
